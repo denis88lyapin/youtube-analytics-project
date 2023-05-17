45702d928a6090d753ebfd0152cedbebe70fd741
Denis_L <denis88lyapin@gmail.com>
2023-05-17 19:57:09 +0300

    fixed Channel in channel.py

--- src/channel.py
+++ src/channel.py
@@ -1,7 +1,6 @@
 import json
 import os
 from googleapiclient.discovery import build
-from helper.youtube_api_manual import youtube
 
 
 class Channel:
@@ -12,10 +11,8 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        self.channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
-
