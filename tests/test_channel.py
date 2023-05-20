15ea8f5316204dd1e3cb93e0f2e4d75fe1e1ec8b
Denis_L <denis88lyapin@gmail.com>
2023-05-20 17:19:10 +0300

    added test new feature

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,26 +1,52 @@
-import json
+import pytest
+from src.channel import Channel
 
 
-def test_channel_init(channel_mock):
-    # Проверка, что инициализация экземпляра класса Channel проходит успешно
-    assert channel_mock.channel_id == 'channel_id'
-    assert channel_mock.channel == {
-        'items': [{
-            'id': 'channel_id',
-            'snippet': {'title': 'Channel Title'},
-            'statistics': {'viewCount': 100, 'subscriberCount': 500}
-        }]
-    }
+@pytest.fixture
+def channel():
+    """Создание экземпляра класса Channel для теста"""
+    return Channel('UC-OVMPlMA3-YCIeg4z5z23A')
 
-def test_channel_print_info(channel_mock, capsys):
-    # Проверка, что метод print_info выводит информацию о канале в консоль
-    channel_mock.print_info()
+
+def test_channel_init(channel):
+    """Проверка инициализации экземпляра класса Channel"""
+    assert channel.channel_id == 'UC-OVMPlMA3-YCIeg4z5z23A'
+    assert channel.title is not None
+    assert channel.description is not None
+    assert channel.url is not None
+    assert channel.subscriber_count is not None
+    assert channel.video_count is not None
+    assert channel.view_count is not None
+
+
+def test_channel_print_info(channel, capsys):
+    """Проверка метода print_info"""
+    channel.print_info()
     captured = capsys.readouterr()
-    assert json.loads(captured.out) == {
-        'items': [{
-            'id': 'channel_id',
-            'snippet': {'title': 'Channel Title'},
-            'statistics': {'viewCount': 100, 'subscriberCount': 500}
-        }]
-    }
+    assert captured.out != ''
 
+
+def test_channel_id(channel):
+    """Проверка метода channel_id"""
+    assert channel.channel_id == 'UC-OVMPlMA3-YCIeg4z5z23A'
+
+
+def test_channel_id_exclusion(channel):
+    """Проверка метода channel_id с исключением"""
+    with pytest.raises(AttributeError) as e:
+        channel.channel_id = "id"
+    assert str(e.value) == "property 'channel_id' of 'Channel' object has no setter"
+
+
+def test_get_service(channel):
+    """Проверка метода get_service"""
+    service = channel.get_service()
+    assert isinstance(service, type(channel.youtube))
+
+
+def test_channel_to_json(channel, tmp_path):
+    """Проверка метода to_json"""
+    file_path = tmp_path / 'channel.json'
+    channel.to_json(str(file_path))
+    assert file_path.exists()
+    assert file_path.read_text() != ''
