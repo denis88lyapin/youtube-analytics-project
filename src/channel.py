6678b5e6f7a2149e318c8c9b7e669d58d9cf879e
Denis_L <denis88lyapin@gmail.com>
2023-05-23 18:35:24 +0300

    added magic methods in channel.py

--- src/channel.py
+++ src/channel.py
@@ -15,10 +15,42 @@ class Channel:
         self.channel = self.youtube.channels().list(id=self._channel_id, part='snippet,statistics').execute()
         self.title = self.channel["items"][0]["snippet"]["title"]
         self.description = self.channel["items"][0]["snippet"]["description"]
-        self.url = self.channel["items"][0]["snippet"]["thumbnails"]["default"]["url"]
+        self.url = f"https://www.youtube.com/channel/{self._channel_id}"
         self.subscriber_count = self.channel["items"][0]["statistics"]["subscriberCount"]
         self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
         self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
+
+    def __str__(self):
+        """Метод вывода данных для пользователя"""
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, other):
+        """Метод сложения атрибутов экземпляров класса"""
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+    def __sub__(self, other):
+        """Метод вычитания атрибутов экземпляров класса"""
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __gt__(self, other):
+        """Метод сравнения атрибутов экземпляров класса (>)"""
+        return int(self.subscriber_count) > int(other.subscriber_count)
+
+    def __ge__(self, other):
+        """Метод сравнения атрибутов экземпляров класса (>=)"""
+        return int(self.subscriber_count) >= int(other.subscriber_count)
+
+    def __lt__(self, other):
+        """Метод сравнения атрибутов экземпляров класса (<)"""
+        return int(self.subscriber_count) < int(other.subscriber_count)
+
+    def __le__(self, other):
+        """Метод сравнения атрибутов экземпляров класса (<=)"""
+        return int(self.subscriber_count) <= int(other.subscriber_count)
+
+    def __eq__(self, other):
+        """Метод сравнения атрибутов экземпляров класса (==)"""
+        return int(self.subscriber_count) == int(other.subscriber_count)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -26,6 +58,7 @@ class Channel:
 
     @property
     def channel_id(self):
+        """Возвращает id канала"""
         return self._channel_id
 
     @channel_id.setter
@@ -34,7 +67,7 @@ class Channel:
 
     @classmethod
     def get_service(cls) -> object:
-        """Возвращающий объект для работы с YouTube API"""
+        """Возвращает объект для работы с YouTube API"""
         return cls.youtube
 
     def to_json(self, file):
