5af0837151ba5d78b166c67aa31d5f7a8f848abe
Denis_L <denis88lyapin@gmail.com>
2023-05-30 18:52:40 +0300

    created fixtures Video, PLVideo

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
-
 from src.channel import Channel
+from src.video import Video, PLVideo
 
 
 @pytest.fixture
@@ -13,3 +13,14 @@ def channel():
 def channel1():
     """Создание экземпляра класса Channel для теста"""
     return Channel('UCwHL6WHUarjGfUM_586me8w')
+
+
+@pytest.fixture
+def video():
+    """Создание экземпляра класса Video для теста"""
+    return Video("AWX4JnAnjBE")
+
+
+@pytest.fixture
+def pl_video():
+    return PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
