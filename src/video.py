4669c5e1e612826a70eeaf5d74e4207374782c19
Denis_L <denis88lyapin@gmail.com>
2023-06-07 22:51:31 +0300

    added exception handling in video.py

--- src/video.py
+++ src/video.py
@@ -1,5 +1,6 @@
 import os
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
 class Video:
@@ -9,13 +10,20 @@ class Video:
 
     def __init__(self, video_id: str) -> None:
         self._video_id = video_id
-        self.response = self.youtube.videos().list(part='snippet,statistics,contentDetails',
-                                                   id=self._video_id).execute()
-        self.title = self.response["items"][0]["snippet"]["title"]
-        self.url = f"https://youtu.be/{self._video_id}"
-        self.view_count = self.response["items"][0]["statistics"]["viewCount"]
-        self.like_count = self.response["items"][0]["statistics"]["likeCount"]
-        self.duration = self.response['items'][0]['contentDetails']['duration']
+        try:
+            self.response = self.youtube.videos().list(part='snippet,statistics,contentDetails',
+                                                       id=self._video_id).execute()
+            self.title = self.response["items"][0]["snippet"]["title"]
+            self.url = f"https://youtu.be/{self._video_id}"
+            self.view_count = self.response["items"][0]["statistics"]["viewCount"]
+            self.like_count = self.response["items"][0]["statistics"]["likeCount"]
+            self.duration = self.response['items'][0]['contentDetails']['duration']
+        except IndexError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+            self.duration = None
 
     def __str__(self) -> str:
         return self.title
@@ -23,6 +31,7 @@ class Video:
 
 class PLVideo(Video):
     """Класс для видео ютуб. Инициализируется по id видео и id плейлиста."""
+
     def __init__(self, video_id: str, playlist_id: str) -> None:
         super().__init__(video_id)
-        self.playlist_id = playlist_id
+        self._playlist_id = playlist_id
